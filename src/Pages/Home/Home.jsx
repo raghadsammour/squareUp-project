d8c61fd99fc5d2779bee88fa301f5ea606284b62
raghadSammour <raghad.sammour.1999@gmail.com>
2025-08-06 16:26:59 +0200

    first commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,9 @@
-import TitleText from "../../Components/TitleText/TitleText"
 
 const Home = () => {
    
   return (
     <>
- <TitleText title={"Our Services"} text={"Transform your brand with our innovative digital solutions that captivate and engage your audience."}/> 
-
+<p>hello from home</p>
     </>
   )
 }
